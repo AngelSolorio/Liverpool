72ea77e8ddcfdecdbc438bddce9778110a56d712
Arturo Guerrero <artupedia@gmail.com>
2014-10-23 12:08:02 -0500

    Change cicstran ip to development Disable soms

--- Classes/Performers/BalanceRequest.m
+++ Classes/Performers/BalanceRequest.m
@@ -15,9 +15,8 @@
 	NSString * soapmessage=[self createEnvelope:requestName forParameters:parameters];
 
 	
-	
+    //http://172.16.204.254:7080/wbi/cicstran //desarrollo
 	//http://172.27.203.25:7080/wbi/cicstran // produccion
-	//http://172.16.204.79:7080/wbi/cicstran //otro
     //172.27.203.25
 	
     NSURL *url=[NSURL URLWithString:@"http://172.16.204.254:7080/wbi/cicstran"];//Web Services desarrollo
